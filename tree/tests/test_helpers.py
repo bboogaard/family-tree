f1d0b584769a7943826756f3a230a51bafab5a4f
bboogaard <padawan@hetnet.nl>
2020-10-04 14:44:54 +0200

    Add more tests for the helpers

--- tree/tests/test_helpers.py
+++ tree/tests/test_helpers.py
@@ -8,45 +8,84 @@ from tree.tests import factories
 
 class TestHelpers(TestCase):
 
+    @classmethod
+    def setUpTestData(cls):
+        cls.top_male = factories.AncestorFactory(gender='m')
+        cls.top_female = factories.AncestorFactory(gender='f')
+        cls.generation_1 = [
+            factories.AncestorFactory(
+                gender='m', father=cls.top_male, mother=cls.top_female
+            ),
+            factories.AncestorFactory(
+                gender='f', father=cls.top_male, mother=cls.top_female
+            )
+        ]
+        factories.MarriageFactory(husband=cls.top_male, wife=cls.top_female)
+
+        cls.spouse_1 = factories.AncestorFactory(gender='f')
+        cls.generation_2 = [
+            factories.AncestorFactory(
+                gender='m', father=cls.generation_1[0], mother=cls.spouse_1
+            ),
+            factories.AncestorFactory(
+                gender='f', father=cls.generation_1[0], mother=cls.spouse_1
+            )
+        ]
+        factories.MarriageFactory(
+            husband=cls.generation_1[0], wife=cls.spouse_1
+        )
+
+        cls.spouse_2 = factories.AncestorFactory(gender='m')
+        cls.generation_extra = [
+            factories.AncestorFactory(
+                gender='m', mother=cls.generation_1[1], father=cls.spouse_2
+            ),
+            factories.AncestorFactory(
+                gender='f', mother=cls.generation_1[1], father=cls.spouse_2
+            )
+        ]
+        factories.MarriageFactory(
+            wife=cls.generation_1[1], husband=cls.spouse_2
+        )
+        factories.LineageFactory(
+            ancestor=cls.top_male, descendant=cls.generation_2[0]
+        )
+
     def test_get_lineage(self):
-        top_male = factories.AncestorFactory(gender='m')
-        top_female = factories.AncestorFactory(gender='f')
-        generation_1 = [
-            factories.AncestorFactory(
-                gender='m', father=top_male, mother=top_female
-            ),
-            factories.AncestorFactory(
-                gender='f', father=top_male, mother=top_female
-            )
+        result = helpers.get_lineage(self.top_male, self.generation_2[0])
+        expected = [
+            self.top_male, self.top_female, self.generation_1[0], self.spouse_1
         ]
-        factories.MarriageFactory(husband=top_male, wife=top_female)
-
-        spouse_1 = factories.AncestorFactory(gender='f')
-        generation_2 = [
-            factories.AncestorFactory(
-                gender='m', father=generation_1[0], mother=spouse_1
-            ),
-            factories.AncestorFactory(
-                gender='f', father=generation_1[0], mother=spouse_1
-            )
-        ]
-        factories.MarriageFactory(husband=generation_1[0], wife=spouse_1)
-
-        spouse_2 = factories.AncestorFactory(gender='m')
-        generation_extra = [
-            factories.AncestorFactory(
-                gender='m', mother=generation_1[1], father=spouse_2
-            ),
-            factories.AncestorFactory(
-                gender='f', mother=generation_1[1], father=spouse_2
-            )
-        ]
-        factories.MarriageFactory(wife=generation_1[1], husband=spouse_2)
-        factories.LineageFactory(ancestor=top_male, descendant=generation_2[0])
-
-        result = helpers.get_lineage(top_male, generation_2[0])
-        expected = [top_male, top_female, generation_1[0], spouse_1]
         self.assertEqual(
             sorted(result, key=operator.attrgetter('id')),
             sorted(expected, key=operator.attrgetter('id'))
         )
+
+    def test_get_parent(self):
+        result = helpers.get_parent(
+            descendant=self.generation_1[0],
+            visible_ancestors=[self.generation_1[0], self.spouse_1]
+        )
+        expected = self.top_male
+        self.assertEqual(result, expected)
+
+    def test_get_parent_doesnotexist(self):
+        result = helpers.get_parent(
+            descendant=self.top_male,
+            visible_ancestors=[
+                self.top_male, self.top_female, self.generation_1[0],
+                self.spouse_1
+            ]
+        )
+        self.assertIsNone(result)
+
+    def test_get_parent_in_visible_ancestors(self):
+        result = helpers.get_parent(
+            descendant=self.generation_1[0],
+            visible_ancestors=[
+                self.top_male, self.top_female, self.generation_1[0],
+                self.spouse_1
+            ]
+        )
+        expected = self.top_male
+        self.assertIsNone(result)
