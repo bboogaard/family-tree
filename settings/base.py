bba6ce9e631d6424b001d1fbe30c84626fdc8771
bboogaard <padawan@hetnet.nl>
2020-10-11 13:45:22 +0200

    Add debug-toolbar

--- settings/base.py
+++ settings/base.py
@@ -49,6 +49,7 @@ PROJECT_APPS = [
 INSTALLED_APPS += PROJECT_APPS
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- settings/development.py
+++ settings/development.py
@@ -1,3 +1,7 @@
 from settings.base import *
 
 DEBUG = True
+
+INSTALLED_APPS += ['debug_toolbar']
+
+INTERNAL_IPS = ['127.0.0.1']
