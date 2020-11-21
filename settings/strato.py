4816804f59f28021a071aea4ea041aeb874ab732
bboogaard <padawan@hetnet.nl>
2020-11-21 19:52:44 +0100

    Sentry integration

--- settings/strato.py
+++ settings/strato.py
@@ -14,3 +14,19 @@ STATIC_ROOT = '/home/bboogaard/media/py-wikkel/staticfiles'
 # E-mail (Add user/pass...)
 EMAIL_HOST = 'smtp.mailtrap.io'
 EMAIL_HOST_PORT = 587
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://bdf806f0125c4e1db409a1fc40f6058d@o480290.ingest.sentry.io/5527044",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
+PRODUCTION_ENV = True
+
--- urls.py
+++ urls.py
@@ -31,3 +31,13 @@ if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls))
     ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+if settings.PRODUCTION_ENV:
+
+    def trigger_error(request):
+        division_by_zero = 1 / 0
+
+    urlpatterns += [
+        path('sentry-debug/', trigger_error),
+    ]
